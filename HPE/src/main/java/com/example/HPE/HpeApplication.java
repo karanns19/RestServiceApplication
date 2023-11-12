29f97f2d47162f764ca48e080f58e02d75d61996
Karan Singh <96336689+karanns19@users.noreply.github.com>
2023-11-12 15:10:22 +0530

    first commit

---
+++ HPE/src/main/java/com/example/HPE/HpeApplication.java
@@ -0,0 +1,13 @@
+package com.example.HPE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HpeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HpeApplication.class, args);
+	}
+
+}
---
+++ HPE/src/test/java/com/example/HPE/HpeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HPE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HpeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
